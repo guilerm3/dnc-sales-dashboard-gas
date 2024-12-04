a77338ea346a8f26cc350b1e12dbc4f08425abce
Guilherme Lucas <127703012+guilerm3@users.noreply.github.com>
2024-12-04 13:05:50 -0300

    Feat material-ui-insercao

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,27 @@
+import { Box, Container, Grid } from "@mui/material"
+import { BannerImage } from "../components/BannerImage.tsx";
+
 function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ alignItems: "center", display: "flex", height: "100vh" }}
+          >
+            <Container maxWidth="sm">
+              <h1>Login</h1>
+            </Container>
+          </Grid>
+
+          <Grid sm={6} sx={{ display: { xs: "none", sm: "block" } }}>
+            <BannerImage />
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
