fb3bd64d341551058bf7cc2d77132621f956f35b
Guilherme Lucas <127703012+guilerm3@users.noreply.github.com>
2024-12-19 11:03:47 -0300

    Feat: Profile-requests

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -54,7 +54,7 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
         url: endpoint,
         method: "GET",
         headers: {
-            'Authorization': `Bearer ${Cookies.get('Authorization')}`,
+          Authorization: `Bearer ${Cookies.get("Authorization")}`,
           ...config?.headers,
         },
         ...config,
@@ -67,9 +67,71 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
     }
   };
 
-  useEffect(() =>{
-    getData()
-  },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
   return { data, loading, error, getData };
+};
+
+export const usePut = <T>(endpoint: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<number | null>(null);
+
+  const putData = async (putData: T, config?: AxiosRequestConfig) => {
+    setData(null);
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axioInstance({
+        url: endpoint,
+        method: "PUT",
+        data: putData,
+        headers: {
+          Authorization: `Bearer ${Cookies.get("Authorization")}`,
+          "Content-Type": "application/json",
+          ...config?.headers,
+        },
+        ...config,
+      });
+      setData(response.data);
+    } catch (e: any) {
+      setError(e.response.status ?? 500);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, putData };
+};
+
+export const useDelete = <T>(endpoint: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const deleteData = async (config?: AxiosRequestConfig) => {
+    setData(null);
+    setLoading(true);
+
+    try {
+      const response = await axioInstance({
+        url: endpoint,
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${Cookies.get("Authorization")}`,
+          ...config?.headers,
+        },
+        ...config,
+      });
+      setData(response.data);
+    } catch (e: any) {
+      throw e.response?.status
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, deleteData }
 };
