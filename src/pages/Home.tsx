7d5e9d94e6832e770006340632df7362000bf763
Guilherme Lucas <127703012+guilerm3@users.noreply.github.com>
2025-01-16 10:31:58 -0300

    Feat: Title to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -76,7 +76,7 @@ function Home() {
                 >
                   {!highlightsLoading && highlightsData && (
                     <>
-                      <StyledH2 className="mb-1">
+                      <StyledH2 className="mb-1" id="total-sales-title">
                         Total de vendas no mês
                       </StyledH2>
                       <StyledH3 className="mb-1" size={40} lineheight={40}>
